25f2e2d8cfa07b8d58c3c038d37a207cc2592156
Victor Wu <wuvictor95@gmail.com>
2023-04-23 20:20:43 -0700

    pt 1

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,33 +1,56 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+	"time"
+)
 
+const (
+	IDLE        = 0
+	IN_PROGRESS = 1
+	COMPLETED   = 2
+)
+
+type Task struct {
+	Status    int
+	WorkerId  string
+	StartedAt time.Time
+}
+
+type MapTask struct {
+	FileName string
+	Task
+}
+
+type ReduceTask struct {
+	Region    int
+	Locations []string
+	Task
+}
 
 type Coordinator struct {
-	// Your definitions here.
-
+	MapTasks             []*MapTask
+	MapTasksRemaining    int
+	ReduceTasks          []*ReduceTask
+	ReduceTasksRemaining int
 }
 
 // Your code here -- RPC handlers for the worker to call.
 
-//
 // an example RPC handler.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
 
-
-//
 // start a thread that listens for RPCs from worker.go
-//
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
@@ -41,30 +64,37 @@ func (c *Coordinator) server() {
 	go http.Serve(l, nil)
 }
 
-//
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
-//
 func (c *Coordinator) Done() bool {
-	ret := false
-
-	// Your code here.
-
-
-	return ret
+	if c.MapTasksRemaining == 0 && c.ReduceTasksRemaining == 0 {
+		return true
+	}
+	return false
 }
 
-//
 // create a Coordinator.
 // main/mrcoordinator.go calls this function.
 // nReduce is the number of reduce tasks to use.
-//
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	c := Coordinator{}
+	c.MapTasks = make([]*MapTask, len(files))
+	c.MapTasksRemaining = len(files)
+	c.ReduceTasks = make([]*ReduceTask, nReduce)
+	c.ReduceTasksRemaining = nReduce
 
-	// Your code here.
+	//Initialize map tasks
+	for i, file := range files {
+		c.MapTasks[i] = &MapTask{FileName: file, Task: Task{Status: IDLE}}
+	}
 
+	//Initialize reduce tasks
+	for i := 0; i < nReduce; i++ {
+		c.ReduceTasks[i] = &ReduceTask{Region: i + 1, Task: Task{Status: IDLE}}
+	}
 
+	fmt.Printf("Coordinator initialized with %v Map Tasks\n", len(files))
+	fmt.Printf("Coordinator initialized with %v Reduce Tasks\n", nReduce)
 	c.server()
 	return &c
 }
